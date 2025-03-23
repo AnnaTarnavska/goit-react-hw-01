4debddcd7a3354a4483c86bdf00a3a8c258f4f37
AnnaTarnavska <anja.t0410@gmail.com>
2025-03-23 20:05:21 +0200

    upd

--- src/components/TransactionHistoryItem/TransactionHistoryItem.jsx
+++ src/components/TransactionHistoryItem/TransactionHistoryItem.jsx
@@ -3,7 +3,7 @@ import styles from "./TransactionHistoryItem.module.css";
 const TransactionHistoryItem = ({ type, amount, currency }) => {
     return (
         <tr className={styles.itemList}>
-            <td className={styles.item}>{type}</td>
+            <td className={styles.item}>{type.charAt(0).toUpperCase()+type.slice(1)}</td>
             <td className={styles.item}>{amount}</td>
             <td className={styles.item}>{currency}</td>
         </tr>
